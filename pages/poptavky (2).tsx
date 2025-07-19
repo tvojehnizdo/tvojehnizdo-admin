13c50834a2f3327d8700fdf0c91440321161fa0f
tvojehnizdo <dveruce@gmail.com>
2025-07-19 04:46:07 +0200

    Delete pages/poptavky (2).tsx

--- pages/poptavky (2).tsx
+++
@@ -1,5 +0,0 @@
-// 🔧 Tato verze obsahuje:
-// - zápis stavu do Google Sheets
-// - logování emailu do listu 'email_log'
-// - PDF generaci (zatím placeholder)
-// - plně responzivní layout s mobilní optimalizací
