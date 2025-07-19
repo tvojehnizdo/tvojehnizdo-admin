2f0876c732b7949a24a6ccfbf584b9a6d4462c82
tvojehnizdo <dveruce@gmail.com>
2025-07-19 04:34:56 +0200

    Add files via upload

---
+++ pages/poptavky (1).tsx
@@ -0,0 +1,5 @@
+// 🔧 Tato verze obsahuje:
+// - zápis stavu do Google Sheets
+// - logování emailu do listu 'email_log'
+// - PDF generaci (zatím placeholder)
+// - plně responzivní layout s mobilní optimalizací
